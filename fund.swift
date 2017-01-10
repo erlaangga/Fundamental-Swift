b626c89f8aeb8e5083a2ae0888209a8b62e00d65
Erlangga Indra Permana <erlangga.ip@gmail.com>
2017-01-10 08:38:01 +0700

    Catatan

--- fund.swift
+++ fund.swift
@@ -1,3 +1,79 @@
+/*
+Operators are unary, binary, or ternary:
+
+- Unary operators operate on a single target (such as -a). Unary prefix operators appear immediately before their target (such as !b), and unary postfix operators appear immediately after their target (such as c!).
+- Binary operators operate on two targets (such as 2 + 3) and are infix because they appear in between their two targets.
+- Ternary operators operate on three targets. Like C, Swift has only one ternary operator, the ternary conditional operator (a ? b : c).
+
+The assignment operator (a = b) initializes or updates the value of a with the value of b.
+
+- Arithmetic Operators
+
+Swift supports the four standard arithmetic operators for all number types:
+
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+
+The addition operator is also supported for String concatenation:
+
+"hello, " + "world"  // equals "hello, world"
+
+Remainder Operator
+
+The remainder operator (a % b) works out how many multiples of b will fit inside a and returns the value that is left over (known as the remainder).
+
+NOTE
+
+The remainder operator (%) is also known as a modulo operator in other languages. However, its behavior in Swift for negative numbers means that it is, strictly speaking, a remainder rather than a modulo operation.
+
+- Unary Plus Operator
+
+The unary plus operator (+) simply returns the value it operates on, without any change:
+
+- Compound Assignment Operators
+
+Like C, Swift provides compound assignment operators that combine assignment (=) with another operation. One example is the addition assignment operator (+=):
+
+var a = 1
+a += 2
+// a is now equal to 3
+
+
+NOTE
+
+The compound assignment operators do not return a value. For example, you cannot write let b = a += 2.
+
+- Ternary Conditional Operator
+
+The ternary conditional operator is a special operator with three parts, which takes the form question ? answer1 : answer2. It is a shortcut for evaluating one of two expressions based on whether question is true or false. If question is true, it evaluates answer1 and returns its value; otherwise, it evaluates answer2 and returns its value.
+
+
+- Nil-Coalescing Operator
+
+The nil-coalescing operator (a ?? b) unwraps an optional a if it contains a value, or returns a default value b if a is nil. The expression a is always of an optional type. The expression b must match the type that is stored inside a.
+
+- Range Operators
+
+Swift includes two range operators, which are shortcuts for expressing a range of values.
+
+Closed Range Operator
+
+The closed range operator (a...b) defines a range that runs from a to b, and includes the values a and b. The value of a must not be greater than b.
+
+The closed range operator is useful when iterating over a range in which you want all of the values to be used, such as with a for-in loop.
+
+- Logical Operators
+
+Logical operators modify or combine the Boolean logic values true and false. Swift supports the three standard logical operators found in C-based languages:
+
+Logical NOT (!a)
+Logical AND (a && b)
+Logical OR (a || b)
+
+*/
+
 // sintaks var digunakan untuk membuat variabel
 // deklarasi variabel pada Swift akan langsung mengidentifikasi datanya jika tidak diberi tipe data
 var myVariable = 42
@@ -38,13 +114,22 @@ let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 var shoppingList = ["catfish", "water", "tulips", "blue paint"]
 shoppingList[1] = "bottle of water"
  
-// dictionary seperti Python dengan struktur [kunci:nilai]
-// dictionary pada Swift mempunyai struktur [TipeDataKunci:TipeDataNilai] sehingga lebih statis
+// dictionary seperti Python dengan bentuk [kunci:nilai]
+// dictionary pada Swift mempunyai tipe data yang homogen, [TipeDataKunci:TipeDataNilai] sehingga lebih statis
 var occupations = [
     "Malcolm": "Captain",
     "Kaylee": "Mechanic",
 ]
 occupations["Jayne"] = "Public Relations"
+print("Mencetak dictionary")
+print("Mencetak dalam bentuk tuple")
+for i in occupations {
+	print(i)
+}
+print("Mencetak key dan value")
+for (i,j) in occupations{
+	print ("\(i) mempunyai nilai \(j)")
+}
 
 // mendeklarasikan array kosong
 let emptyArray = [String]()
